23d9a7e445b737ebff6abba29c925d9cfefe7433
Brenobn <brenobragabn@gmail.com>
2025-03-10 11:03:07 -0300

    Creating Insert and Delete Routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
+import controllerRide from "./controllers/controller.ride.js";
 
 const app = express();
 
+// Middlewares...
 app.use(express.json());
 app.use(cors());
 
+// Routes...
+app.get("/rides", controllerRide.List);
+app.post("/rides", controllerRide.Insert);
+app.delete("/rides/:ride_id", controllerRide.Delete);
+
 app.listen(3001, () => {
-  console.log("App running port 3001");
+  console.log("App running - Port 3001");
 });
