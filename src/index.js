989180c7dd6337f2a1981e23cd49cf9a04f326e8
Brenobn <brenobragabn@gmail.com>
2025-03-14 04:29:08 -0300

    Creating the Accept, Cancel, DriverList and RideDetail routes with their respective methods

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ app.get("/rides", controllerRide.List);
 app.post("/rides", controllerRide.Insert);
 app.delete("/rides/:ride_id", controllerRide.Delete);
 app.put("/rides/:ride_id/finish", controllerRide.Finish);
+app.put("/rides/:ride_id/accept", controllerRide.Accept);
+app.put("/rides/:ride_id/cancel", controllerRide.Cancel);
+app.get("/rides/drivers/:driver_user_id", controllerRide.DriverList);
+app.get("/rides/:ride_id", controllerRide.RideDetail);
 
 app.listen(3001, () => {
   console.log("App running - Port 3001");
